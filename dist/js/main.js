506626d5381029584657a9f779069f9d127a6a57
auziak <auziak90@gmail.com>
2019-07-25 21:26:04 +0300

    JS, menu button & rotate

---
+++ dist/js/main.js
@@ -0,0 +1,28 @@
+// Select DOM Items
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuBranding = document.querySelector(".menu-branding");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".nav-item");
+
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach(i => i.classList.add("show"));
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach(i => i.classList.remove("show"));
+    showMenu = false;
+  }
+}
