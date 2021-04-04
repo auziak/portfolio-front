6e45deb1ee162e730d185c05df20add5bcf48b41
auziak <auziak90@gmail.com>
2021-04-04 18:18:57 +0300

    make my work images clickable

---
+++ dist/js/work.js
@@ -0,0 +1,8 @@
+const youTranslate = document.getElementById("you-translate");
+const shoppingList = document.getElementById("shopping-list");
+
+const youTranslateSite = "https://auziak.github.io/you-translate";
+const shoppingListSite = "https://blooming-tundra-96945.herokuapp.com/";
+
+youTranslate.href = youTranslateSite;
+shoppingList.href = shoppingListSite;
